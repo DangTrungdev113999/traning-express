60c59b57cc7e33342a72656160a1d6e4a7fd6936
dangtrungdev <dangtrungdev113999@gmail.com>
2019-09-27 10:57:35 +0700

    fix part 1

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const productsRoute = require("./routes/routeProduct");
 const app = express();
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("./src/public"))
 
